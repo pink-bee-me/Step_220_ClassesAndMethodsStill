3128661294a3bd5a01ae389fffa349b226763113
LaurieSue <pink.bee.me@outlook.com>
2020-10-13 14:04:07 -0500

    Add project files.

---
+++ Step_220_ClassesAndMethodsStill/MathProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Step_220_ClassesAndMethodsStill
+{
+    class MathProblem
+    {
+
+
+        public int a { get; set; }
+        public int b { get; set; }
+        public int nums { get; set; }
+        public int x { get; set; }
+
+
+        public int Add(int a, int b = 0)
+        {
+            int x = a + b + 100;
+            return x;
+        }
+
+        public int Add(int a)
+        {
+            int x = a + 100;
+            return x;
+        }
+    }
+}
---
+++ Step_220_ClassesAndMethodsStill/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_220_ClassesAndMethodsStill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter two numbers (The second number is optional)");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+           
+            
+                MathProblem problem = new MathProblem();
+                Console.WriteLine(problem.Add(a, b));
+                Console.ReadLine();
+        }
+    }}
+
