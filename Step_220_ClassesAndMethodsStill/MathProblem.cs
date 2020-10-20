41277747f41e0b748602d5e3115801d27fb266ce
LaurieSue <pink.bee.me@outlook.com>
2020-10-20 16:14:01 -0500

    used one Add method with the int b set to equal zero which made the (b) integer optional thereby you can accomplish all goals with one method

--- Step_220_ClassesAndMethodsStill/MathProblem.cs
+++ Step_220_ClassesAndMethodsStill/MathProblem.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
@@ -9,25 +11,18 @@ using System.Windows.Markup;
 namespace Step_220_ClassesAndMethodsStill
 {
     class MathProblem
+
+
+
     {
-
-
-        public int a { get; set; }
-        public int b { get; set; }
-        public int nums { get; set; }
-        public int x { get; set; }
-
-
+        
         public int Add(int a, int b = 0)
         {
-            int x = a + b + 100;
-            return x;
+            X = a + b + 100;
+            return X;
         }
 
-        public int Add(int a)
-        {
-            int x = a + 100;
-            return x;
-        }
+
+       public int X { get; set; }
     }
-}
+    }
--- Step_220_ClassesAndMethodsStill/Program.cs
+++ Step_220_ClassesAndMethodsStill/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,13 +16,28 @@ namespace Step_220_ClassesAndMethodsStill
 
 
             Console.WriteLine("Enter two numbers (The second number is optional)");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
            
-            
-                MathProblem problem = new MathProblem();
-                Console.WriteLine(problem.Add(a, b));
-                Console.ReadLine();
+          
+
+            MathProblem problem = new MathProblem();
+
+
+            int a = Convert.ToInt32(num1);
+            if (num2 == "")
+            {
+                int b = 0;
+                Console.WriteLine("This is your new math problem: \n " + num1 + " + 100 = " + (problem.Add(a,b)));
+            }
+
+            else
+            {
+                int b = Convert.ToInt32(num2);
+                Console.WriteLine("This is your new math problem: \n " + num1 + " + " + num2 + " + 100 = " + (problem.Add(a, b)));
+            }
+            Console.ReadLine();
         }
-    }}
+    }
+}
 
